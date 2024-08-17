90801c642b8c3653894c50d4d03ea9fbbfc9caab
afiq-ekkery <afiq-ekkeri@users.noreply.github.com>
2024-08-17 14:50:07 +0530

    initial setup

---
+++ src/screens/RequestsScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RequestsScreen = () => {
+  return (
+    <View>
+      <Text>RequestsScreen</Text>
+    </View>
+  )
+}
+
+export default RequestsScreen
