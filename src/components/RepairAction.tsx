86e6c94fe22dd2cda110a6165e680e7402dbe149
rizwaan-ansari <rizwaan@oromedialab.com>
2024-09-28 18:18:28 +0530

    Added options for launchCameera and launchGallery

--- src/components/RepairAction.tsx
+++ src/components/RepairAction.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import { TouchableOpacity, View } from 'react-native'
+import { Alert, TouchableOpacity, View } from 'react-native'
 import FastImage from 'react-native-fast-image'
-import { Asset, ImageLibraryOptions, launchImageLibrary } from 'react-native-image-picker'
+import { Asset, CameraOptions, ImageLibraryOptions, launchCamera, launchImageLibrary } from 'react-native-image-picker'
 import { TextInput } from 'react-native-paper'
 import { SvgCross, SvgUpload } from '../assets/images'
 import COLOR_PALLETE from '../utils/ColorConstant'
@@ -28,6 +28,51 @@ const RepairAction = () => {
 
     const [selectedImages, setSelectedImages] = useState<string[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const showImagePickerOption = () => {
+        Alert.alert(
+            "Select Image",
+            "Choose an option",
+            [
+                {
+                    text: "Take Photo",
+                    onPress: openCamera,
+                },
+                {
+                    text: "Choose from Gallery",
+                    onPress: openImagePicker,
+                },
+                {
+                    text: 'Cancel',
+                    style: 'cancel'
+                }
+            ]
+        )
+    }
+
+    const openCamera = () => {
+        const options: CameraOptions = {
+            mediaType: 'photo',
+            saveToPhotos: true,
+            maxHeight: 2000,
+            maxWidth: 2000
+        }
+
+        launchCamera(options, response => {
+            setIsLoading(false);
+            if (response.didCancel) {
+                console.log("User cancelled camera");
+            } else if (response.errorCode) {
+                console.log('Camera Error: ', response.errorMessage);
+            } else if (response.assets && response.assets.length > 0) {
+                const newImages = response.assets
+                    .map((asset: Asset) => asset.uri)
+                    .filter((uri): uri is string => uri !== undefined)
+
+                    setSelectedImages(prevImages => [...prevImages, ...newImages])
+            }
+        })
+    }
 
     // Function to open the image picker
     const openImagePicker = () => {
@@ -73,7 +118,7 @@ const RepairAction = () => {
                 multiple={false}
                 options={REPAIR_STATUS.map((item) => { return { label: item.status, value: item.id } })}
             />
-            <TouchableOpacity onPress={openImagePicker}>
+            <TouchableOpacity onPress={showImagePickerOption}>
                 <View className='h-[210px] justify-center items-center bg-white20' style={{ borderRadius: 10, borderWidth: 1, borderStyle: 'dashed', borderColor: 'rgba(0, 0, 0, 0.1)', marginTop: 15, }}>
                     <SvgUpload />
                     <Txt fontSize={'sm'} textAlign={'center'} className='mt-[10px]'>Upload Image or Video</Txt>
